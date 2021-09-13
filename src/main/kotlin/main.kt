8549ff6c4778e64d44639e14ad398455667f0d3e
LucasDev13 <lucaspontes140@gmail.com>
2021-09-13 14:42:29 -0300

    Iterando por um array.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,20 @@
 fun main() {
-    val idades = IntArray(5)
-    idades[0] = 25
-    idades[1] = 19
-    idades[2] = 33
-    idades[3] = 20
-    idades[4] = 55
+    val idades: IntArray = intArrayOf(25, 19, 33, 20, 55, 40)
 
-    var maiorIdade = 0
+    var maiorIdade = Int.MIN_VALUE
     for(idade in idades){
         if (idade > maiorIdade){
             maiorIdade = idade
         }
     }
-
     println(maiorIdade)
+
+    var menorIdade = Int.MAX_VALUE
+    idades.forEach { idade ->
+        if(idade < menorIdade){
+            menorIdade = idade
+        }
+    }
+
+    println(menorIdade)
 }
