4d029f2d59963f281462721169eaca549a901805
LucasDev13 <lucaspontes140@gmail.com>
2021-09-15 14:59:27 -0300

    Trabalhando com listas.

---
+++ src/main/kotlin/list/ListaDeLivros.kt
@@ -0,0 +1,48 @@
+package list
+
+fun main(){
+    val livro1 = Livro(
+        titulo = "Grande Sertão 1",
+        autor = "Não tem",
+        anoPublicacao = 1995
+    )
+
+    val livro2 = Livro(
+        titulo = "Grande Sertão 2",
+        autor = "Não tem",
+        anoPublicacao = 1996,
+        editora = "Editora 2"
+    )
+
+    val livro3 = Livro(
+        titulo = "Grande Sertão 3",
+        autor = "Não tem",
+        anoPublicacao = 1997
+    )
+
+    val livro4 = Livro(
+        titulo = "Grande Sertão 4",
+        autor = "Não tem",
+        anoPublicacao = 1998,
+        editora = "Editora 4"
+    )
+    //quando se trabalha com list é sempre com objetos
+    //MutableList<Livro> -> o que fica dentro de <> é o tipo que indica que estamos usando Generics(cria um objeto do tipo genérico)
+    //e todos os métodos da mutableList vai se adaptar ao tipo que é passado em <>
+    //as listas tem valores dinâmicos e os array não, é definido já no momento de criação do array o seu tamanho
+    val livros:MutableList<Livro> = mutableListOf(livro1, livro2, livro3, livro4)
+
+    livros.add(
+        Livro(
+            titulo = "Grande Sertão 5",
+            autor = "Não tem",
+            anoPublicacao = 1999
+        )
+    )
+
+    println("Livros> $livros")
+
+    livros.remove(livro1)
+
+    println("Livro 1 removido> $livros")
+}
---
+++ src/main/kotlin/list/Livro.kt
@@ -0,0 +1,13 @@
+package list
+
+data class Livro(
+    //atributos definidos no construtor primário, e para cadata atributo,ele vai criar em tempo de compilação
+    //os métodos toString, equals e hashCode tudo em tempo de compilação
+    val titulo: String,
+    val autor: String,
+    val anoPublicacao: Long,
+    val editora: String? = null
+){
+    //data class é um tipo especial no kotlin, que ela fornece além das caracteristis normais de uma classe
+    //ela já fornece algumas funções automáticamente
+}
