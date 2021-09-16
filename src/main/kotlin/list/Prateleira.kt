9fec6856715825f2df540cedf0a2dfba380ad001
LucasDev13 <lucaspontes140@gmail.com>
2021-09-16 15:51:50 -0300

    Listas imutáveis.

--- src/main/kotlin/list/Prateleira.kt
+++ src/main/kotlin/list/Prateleira.kt
@@ -4,13 +4,11 @@ data class Prateleira(
     val genero: String,
     val livros: MutableList<Livro>
 ){
-    fun organizarPorAutor(): MutableList<Livro>{
-        livros.sortBy { it.autor}
-        return livros
+    fun organizarPorAutor(): List<Livro>{
+        return livros.sortedBy { it.autor }
     }
 
-    fun organizarPorAnoPublicacao(): MutableList<Livro>{
-        livros.sortBy { it.anoPublicacao }
-        return livros
+    fun organizarPorAnoPublicacao(): List<Livro>{
+        return livros.sortedBy { it.anoPublicacao }
     }
 }
